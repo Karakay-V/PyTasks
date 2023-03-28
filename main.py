88e3a7392041c999a6b0ac01b92069f681e40072
karakenza <hitechnic00074220@gmail.com>
2023-03-28 10:29:49 +0300

    change workplace

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyTasks.taskFirst import taskFirst
+from PyTasks.taskSecond import taskSecond
+from PyTasks.taskThird import taskThird
+
+#---------RUN-TIME---------#
+
+taskFirst()
+
+#---------RUN-TIME---------#
